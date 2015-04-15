e7f5f5ce884bad012bdbd1b008988e34ffec0fc3
Chris Campo <ccampo133@users.noreply.github.com>
2015-04-14 21:46:15 -0400

    Added initial scaffolding

---
+++ src/main/java/oauth2demo/SpringBootOauth2DemoApplication.java
@@ -0,0 +1,12 @@
+package oauth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauth2DemoApplication.class, args);
+    }
+}
