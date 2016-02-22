abcae4c265136947445308ce6819c635bdc0780b
Chris Campo <ccampo133@users.noreply.github.com>
2016-02-22 12:56:37 -0500

    Add CORs demo

--- src/main/java/oauth2demo/Application.java
+++ src/main/java/oauth2demo/Application.java
@@ -2,11 +2,28 @@ package oauth2demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addExposedHeader("Authorization");
+        config.addExposedHeader("Content-Type");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
--- src/test/java/oauth2demo/ApplicationTests.java
+++ src/test/java/oauth2demo/ApplicationTests.java
@@ -2,17 +2,35 @@ package oauth2demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
+@WebIntegrationTest(randomPort = true)
 public class ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    private final TestRestTemplate restTemplate = new TestRestTemplate();
 
+    @Value("${local.server.port}")
+    int port;
+
+    @Test
+    public void testCors() {
+        final ResponseEntity<Void> response = restTemplate.exchange("http://localhost:" + port + "/oauth/token",
+                HttpMethod.OPTIONS, null, Void.class);
+        final HttpHeaders headers = response.getHeaders();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(headers.getAccessControlAllowOrigin());
+    }
 }
